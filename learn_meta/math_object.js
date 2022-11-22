901a1dc7ac4493f7839bebfbc503b949cbd245c6
Xiaoqi Zhao <xiaoqi.zhao@volvo.com>
2022-11-21 19:55:08 -0500

    Update math_object.js

--- learn_meta/math_object.js
+++ learn_meta/math_object.js
@@ -4,18 +4,21 @@ console.log(Math.E);
 console.log(Math.LN2); */
 
 // Rounding methods
-var num = 1.567;
-console.log(num);
+/* var num = 1.567;
+console.log(num); */
 /* console.log("Ceil", Math.ceil(num));
 console.log("Floor", Math.floor(num));
 console.log("Round", Math.round(num));
 console.log("Trunc", Math.trunc(num)); */
 
 // Arithmetic and calculus methods
-console.log(Math.pow(num,3));
+/* console.log(Math.pow(num,3));
 console.log(Math.sqrt(num));
 console.log(Math.cbrt(27));
 console.log(Math.abs(-10));
 console.log(Math.log(num));
 console.log(Math.log2(num));
-console.log(Math.log10(num));
+console.log(Math.log10(num)); */
+
+// Random method
+console.log(Math.random());
