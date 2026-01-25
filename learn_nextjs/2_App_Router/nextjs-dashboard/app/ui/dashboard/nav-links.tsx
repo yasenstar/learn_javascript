f36b7083319ad96d861cd4f0dd3e477cf05e0171
Xiaoqi Zhao <xiaoqi.zhao@volvo.com>
2026-01-25 21:57:05 +0800

    finish chapter 4

--- learn_nextjs/2_App_Router/nextjs-dashboard/app/ui/dashboard/nav-links.tsx
+++ learn_nextjs/2_App_Router/nextjs-dashboard/app/ui/dashboard/nav-links.tsx
@@ -2,6 +2,7 @@ import {
   UserGroupIcon,
   HomeIcon,
   DocumentDuplicateIcon,
+  TruckIcon,
 } from '@heroicons/react/24/outline';
 
 // Map of links to display in the side navigation.
@@ -14,6 +15,7 @@ const links = [
     icon: DocumentDuplicateIcon,
   },
   { name: 'Customers', href: '/dashboard/customers', icon: UserGroupIcon },
+  { name: 'Test', href: '/dashboard/test', icon: TruckIcon },
 ];
 
 export default function NavLinks() {
