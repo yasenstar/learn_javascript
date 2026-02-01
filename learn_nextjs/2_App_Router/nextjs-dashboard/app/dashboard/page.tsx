ae39a0c81b31c578460f0568d04bcdcd42f74018
Xiaoqi Zhao <xiaoqi.zhao@volvo.com>
2026-02-01 11:35:49 +0800

    add 7.1 and 7.2

--- learn_nextjs/2_App_Router/nextjs-dashboard/app/dashboard/page.tsx
+++ learn_nextjs/2_App_Router/nextjs-dashboard/app/dashboard/page.tsx
@@ -1,3 +1,14 @@
-export default function Page() {
-    return <div><p>Dashboard Page created by Yasen</p></div>;
+import { lusitana } from '@/app/ui/fonts'
+
+export default async function Page() {
+    // return <div><p>Dashboard Page created by Yasen, Hello World!</p></div>;
+    return (
+        <main>
+            <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
+                Dashboard
+            </h1>
+            <div></div>
+            <div></div>
+        </main>
+    );
 }
