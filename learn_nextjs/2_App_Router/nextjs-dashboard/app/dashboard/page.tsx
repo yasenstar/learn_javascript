58639428d208fac9dd5df6e8ce719e90e901c9ed
Xiaoqi Zhao <xiaoqi.zhao@volvo.com>
2026-02-01 13:55:16 +0800

    add 7.4

--- learn_nextjs/2_App_Router/nextjs-dashboard/app/dashboard/page.tsx
+++ learn_nextjs/2_App_Router/nextjs-dashboard/app/dashboard/page.tsx
@@ -1,6 +1,11 @@
-import { lusitana } from '@/app/ui/fonts'
+import RevenueChart from '@/app/ui/dashboard/revenue-chart';
+import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
+import { lusitana } from '@/app/ui/fonts';
+import { fetchRevenue, fetchLatestInvoices } from '@/app/lib/data';
 
 export default async function Page() {
+    const revenue = await fetchRevenue();
+    const latestInvoices = await fetchLatestInvoices();
     // return <div><p>Dashboard Page created by Yasen, Hello World!</p></div>;
     return (
         <main>
@@ -8,7 +13,10 @@ export default async function Page() {
                 Dashboard
             </h1>
             <div></div>
-            <div></div>
+            <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
+                <RevenueChart revenue={revenue} />
+                <LatestInvoices latestInvoices={latestInvoices} />
+            </div>
         </main>
     );
 }
