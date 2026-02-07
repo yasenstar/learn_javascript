d6f485b0972f6fb27db2cc214d227344f2ce4538
Xiaoqi Zhao <xiaoqi.zhao@volvo.com>
2026-02-07 17:34:16 +0800

    add 9.2.2 (3)

--- learn_nextjs/2_App_Router/nextjs-dashboard/app/dashboard/(overview)/page_before_9.2.3.tsx
+++ learn_nextjs/2_App_Router/nextjs-dashboard/app/dashboard/(overview)/page_before_9.2.3.tsx
@@ -2,11 +2,10 @@ import { Card } from '@/app/ui/dashboard/cards';
 import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitana } from '@/app/ui/fonts';
-import { fetchLatestInvoices, fetchCardData } from '@/app/lib/data';
-import { Suspense } from 'react';
-import { RevenueChartSkeleton } from '@/app/ui/skeletons';
+import { fetchRevenue, fetchLatestInvoices, fetchCardData } from '@/app/lib/data';
 
 export default async function Page() {
+    const revenue = await fetchRevenue();
     const latestInvoices = await fetchLatestInvoices();
     const {
         numberOfInvoices,
@@ -27,9 +26,7 @@ export default async function Page() {
                 <Card title="Total Customer" value={numberOfCustomers} type="customers" />
             </div>
             <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
-                <Suspense fallback={<RevenueChartSkeleton />}>
-                    <RevenueChart />
-                </Suspense>
+                <RevenueChart revenue={revenue} />
                 <LatestInvoices latestInvoices={latestInvoices} />
             </div>
         </main>
