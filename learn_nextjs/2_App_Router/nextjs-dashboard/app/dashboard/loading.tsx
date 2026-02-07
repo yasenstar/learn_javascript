2f1920fa06b15c4cb3930f0a91e2b27ff21acec2
Xiaoqi Zhao <xiaoqi.zhao@volvo.com>
2026-02-07 15:43:20 +0800

    add 9.2 (1)

--- learn_nextjs/2_App_Router/nextjs-dashboard/app/dashboard/loading.tsx
+++ learn_nextjs/2_App_Router/nextjs-dashboard/app/dashboard/loading.tsx
@@ -1,3 +1,6 @@
+import DashboardSkeleton from '@/app/ui/skeletons';
+
 export default function Loading() {
-    return <div>Loading..., be patient, thanks!</div>
+    // return <div>Loading..., be patient, thanks!</div>;
+    return <DashboardSkeleton />;
 }
