027fdb92cf3b8e357a2a84756839d35b496fe61e
Xiaoqi Zhao <xiaoqi.zhao@volvo.com>
2026-02-15 20:31:28 +0800

    update

--- learn_nextjs/2_App_Router/nextjs-dashboard/app/dashboard/invoices/[id]/edit/page.tsx
+++ learn_nextjs/2_App_Router/nextjs-dashboard/app/dashboard/invoices/[id]/edit/page.tsx
@@ -1,1 +1,26 @@
-export default function page() {}
+import Form from '@/app/ui/invoices/edit-form';
+import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
+import { fetchCustomers, fetchInvoiceById } from '@/app/lib/data';
+
+export default async function page(
+    props: { params: Promise<{ id: string }>}
+) {
+    const params = await props.params;
+    const id = params.id;
+    return (
+        <main>
+            <Breadcrumbs
+                breadcrumbs={[
+                    { label: 'Invoices', href: '/dashboard/invoices'},
+                    {
+                        label: 'Edit Invoice',
+                        href: `/dashboard/invoices/${id}/edit`,
+                        active: true,
+                    },
+                ]}
+            >                
+            </Breadcrumbs>
+            <Form invoice={invoice} customers={customers}></Form>
+        </main>
+    );
+}
