a650e15597993b97be0043fc135e3cc667a31905
Xiaoqi Zhao <xiaoqi.zhao@volvo.com>
2026-02-15 14:51:56 +0800

    add 11.4 (4)

--- learn_nextjs/2_App_Router/nextjs-dashboard/app/lib/actions.ts
+++ learn_nextjs/2_App_Router/nextjs-dashboard/app/lib/actions.ts
@@ -1,10 +1,30 @@
 'use server';
 
+import { z } from 'zod';
+
+const FormSchema = z.object({
+    id: z.string(),
+    customerId: z.string(),
+    amount: z.coerce.number(),
+    status: z.enum(['pending','paid']),
+    date: z.date(),
+});
+
+const CreateInvoice = FormSchema.omit({id:true, date:true});
+
 export async function createInvoice(formData: FormData) {
-    const rawFormData = {
+    // const rawFormData = {
+    //     customerId: formData.get('customerId'),
+    //     amount: formData.get('amount'),
+    //     status: formData.get('status'),
+    // };
+    const { customerId, amount, status } = CreateInvoice.parse({
         customerId: formData.get('customerId'),
         amount: formData.get('amount'),
         status: formData.get('status'),
-    };
-    console.log(rawFormData);
+    });
+    const amountInCents = amount * 100;
+    const date = new Date().toISOString().split('T')[0];
+    console.log(customerId, amount, amountInCents, status, date);
+    console.log(typeof amount);
 }
